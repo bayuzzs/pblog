928c71319e89408c67b9124630f7f71b36350f01
Rahel Simanjuntak <aahelsimanjuntak@gmail.com>
2024-04-06 15:30:07 +0700

    update data master

--- database/migrations/2024_04_06_071033_create_datamasters_table.php
+++ database/migrations/2024_04_06_071033_create_datamasters_table.php
@@ -11,9 +11,32 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('datamasters', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('hs', function (Blueprint $table) {
+            $table->integer('kodeHS', 10)->primary();
+            $table->string('uraianBarangBahasa');
+            $table->string('uraianBarangEnglish');
+            $table->boolean('isLartas');
+        });
+
+        Schema::create('valuta', function (Blueprint $table) {
+            $table->string('kodeValuta', 3)->primary();
+            $table->string('namaValuta');
+            $table->integer('kurs');
+        });
+
+        Schema::create('jeniskemasan', function (Blueprint $table) {
+            $table->string('kodeKemasan', 2)->primary();
+            $table->string('namaKemasan');
+        });
+
+        Schema::create('jenisdokumen', function (Blueprint $table) {
+            $table->string('kodeJenisDokumen', 5)->primary();
+            $table->string('namaDokumen');
+        });
+
+        Schema::create('pelabuhan', function (Blueprint $table) {
+            $table->string('kodeKantor', 6)->primary();
+            $table->string('namaKantor');
         });
     }
 
@@ -22,6 +45,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('datamasters');
+        Schema::dropIfExists('hs');
+        Schema::dropIfExists('valuta');
+        Schema::dropIfExists('jeniskemasan');
+        Schema::dropIfExists('jenisdokumen');
+        Schema::dropIfExists('pelabuhan');
     }
 };
