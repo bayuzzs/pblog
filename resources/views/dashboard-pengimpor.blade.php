40a5dae8f0a0b710e339e314b07914422af663e8
Bayu Maulana <bayufransdo@gmail.com>
2024-06-16 18:04:50 +0700

    i hope this is last

--- resources/views/dashboard-pengimpor.blade.php
+++ resources/views/dashboard-pengimpor.blade.php
@@ -3,6 +3,6 @@
 @section('main-content')
 		<div
 				class="custom-scrollbar h-[calc(100vh-150px)] w-full overflow-auto rounded-2xl bg-white p-5 shadow-xl dark:bg-gray-800">
-				<img src="{{ asset('images/poster.webp') }}" alt="Poster Polibatam Logistik" class="rounded-xl">
+				<img src="{{ asset('images/Poster.webp') }}" alt="Poster Polibatam Logistik" class="rounded-xl">
 		</div>
 @endsection
